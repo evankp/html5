b867c8a72d95e267c89f5ee827a8e3a9cda851ae
Evan Kemp <ekp.web@gmail.com>
2018-09-11 22:29:47 -0700

    geolocation and watchpostion

--- js/geolocate.js
+++ js/geolocate.js
@@ -1,17 +1,8 @@
 $(function () {
-
+    
     const SFOffice = {
         lat: 37.776176,
-        long: -122.396495
-    }
-
-    // Google maps api
-
-    window.initMap = () => {
-        map = new google.map.Map(document.getElementById('map'), {
-            center: SFOffice,
-            zoom: 8
-        })
+        lng: -122.396495
     }
 
     // geolocate functions
@@ -25,21 +16,62 @@ $(function () {
         const calcDistance = (start, dest) => {
             return Math.round(Math.acos(Math.sin(radianConvert(start.lat)) * Math.sin(radianConvert(dest.lat))
                 + Math.cos(radianConvert(start.lat)) * Math.cos(radianConvert(dest.lat)) *
-                Math.cos(radianConvert(start.long) - dest.long)) * 6371)
+                Math.cos(radianConvert(start.lng) - dest.lng)) * 6371)
         }
 
         geo.getCurrentPosition(function (position) {
             let startCoords = {
                 lat: position.coords.latitude,
-                long: position.coords.longitude
+                lng: position.coords.longitude,
+                accuracy: position.coords.accuracy
             }
 
             $('#lat').text(startCoords.lat)
-            $('#long').text(startCoords.long)
+            $('#long').text(startCoords.lng)
+            $('#accuracy').text(startCoords.accuracy)
 
             $('#distance').text(calcDistance(startCoords, SFOffice))
+            var trackMap = new google.maps.Map(document.getElementById('track-map'), {
+                    zoom: 12,
+                    center: {lat: startCoords.lat, lng: startCoords.lng}
+            })
+
+            var trackMarker = new google.maps.Marker({
+                position: {lat: startCoords.lat, lng: startCoords.lng},
+                map: trackMap,
+                title: 'Your position',
+                clickable: true
+            })
+
+            var trackInfoWindow = new google.maps.InfoWindow({
+                content: 'Your position',
+                position: {lat: startCoords.lat, lng: startCoords.lng}
+            })
+
+            trackMarker.addListener("click", () => infoWindow.open(map, marker))
+
+            geo.watchPosition(function () {
+                // success
+                var newPosition = {lat: position.coords.latitude, lng: position.coords.longitude}
+
+                trackMap.center = newPosition
+                trackMarker.position = newPosition
+                trackInfoWindow.position = newPosition
+            }, function (e) {
+                // error
+                errorAlert(e)
+            })
         }, function (e) {
-            switch (e.code) {
+            errorAlert(e)
+        }, {timeout: 60000, maximumAge: 200})
+
+    } else {
+        alert('Geolocation features are, unfortunately not supported on your browser')
+        window.location.href = "/"
+    }
+
+    function errorAlert(e) {
+        switch (e.code) {
                 case 1:
                     alert('Location permission denied')
                     window.location.href = '/'
@@ -59,9 +91,5 @@ $(function () {
                     alert('Unknown error')
                     window.location.href = '/'
             }
-        })
-    } else {
-        alert('Geolocation features are, unfortunately not supported on your browser')
-        window.location.href = "/"
     }
 })
